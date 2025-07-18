a2821f25d3bf5439d2f7d9b97dde75d096112e99
Hassan-code1 <2024kuec2011@iiitkota.ac.in>
2025-07-18 09:28:39 +0530

    owner route and config

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const ownerModel = require("../models/owner-model");
 
 router.get("/", function(req, res){
     res.send("hey");
 });
+
+if(process.env.NODE_ENV === "development"){
+    router.post("/create", async function (req, res) {
+      let owners = await ownerModel.find();
+      if(owners.length > 0){
+        return res.status(501).send("Owner is already their!");
+      }
+    //   let {fullname, email, password} = req.body;
+    //   let createdOwner = await ownerModel.create({
+    //     fullname,
+    //     email,
+    //     password
+    //   });
+    //   res.status(201).send(createdOwner);
+    res.send("Create Owner");
+    });
+}
+
 
 module.exports = router;
